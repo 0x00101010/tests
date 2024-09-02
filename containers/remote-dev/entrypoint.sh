17802ee8f48bbbed6b1bf30cb435ae02bd1e802e
Francis Li <francis.li@coinbase.com>
2024-09-01 17:15:52 -0700

    Update

--- containers/remote-dev/entrypoint.sh
+++ containers/remote-dev/entrypoint.sh
@@ -5,8 +5,6 @@ set -eu
     echo "Starting rust"
     /app/remote-dev &
 )
-RUST_PID=$!
-echo "Rust program started with PID: $RUST_PID"
 
 echo "Starting lldb-server"
 exec lldb-server platform --listen *:1234 --log-file /tmp/lldb-server.log --log-channels "lldb all" --server
