bfa85394692c7209e02d2bf772435717267c768a
Francis Li <francis.li@coinbase.com>
2024-09-01 19:34:10 -0700

    Update

--- containers/remote-dev/entrypoint.sh
+++ containers/remote-dev/entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -eu
 
-(
-    echo "Starting rust"
-    /app/remote-dev &
-)
+exec lldb-server gdbserver 0.0.0.0:1234 -- /app/remote-dev
 
-echo "Starting lldb-server"
-exec lldb-server platform --listen *:1234 --log-file /tmp/lldb-server.log --log-channels "lldb all" --server
+# (
+#     echo "Starting rust"
+#     /app/remote-dev &
+# )
+
+# echo "Starting lldb-server"
+# exec lldb-server platform --listen *:1234 --log-file /tmp/lldb-server.log --log-channels "lldb all" --server
